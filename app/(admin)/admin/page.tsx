9817dd1f14bb84775400a8c8e20bbb1bc0784538
Lukas Ružauskas <nequadora@gmail.com>
2023-05-25 11:03:54 +0300

    A bit of structure.

---
+++ app/(admin)/admin/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+import Login from "@/components/admin/Login";
+import { supabase } from "@/lib/supabaseClient";
+import { FormEvent, useState } from "react";
+import { useForm } from "react-hook-form";
+import ErrorToast from "@/components/common/ErrorToast";
+import loginAction from "@/components/actions/admin/loginAction";
+
+type FormData = {
+  email: string;
+  password: string;
+};
+
+export default async function Page() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const [showError, setShowError] = useState(false);
+
+  const submitForm = handleSubmit((data) => console.log(data));
+
+  return (
+    <div className="w-1/2 mx-auto">
+      <form onSubmit={submitForm}>
+        {showError ? (
+          <ErrorToast text={"Neteisingi prisijungimo duomenys"} />
+        ) : (
+          ""
+        )}
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="user@email.com"
+            required
+            {...register("email")}
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your password
+          </label>
+          <input
+            type="password"
+            id="password"
+            {...register("password")}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+          />
+        </div>
+        <input
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        />
+      </form>
+    </div>
+  );
+}
