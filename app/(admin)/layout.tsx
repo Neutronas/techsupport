e91bdf399b2cb307d270baeb20e6abc7ec4208da
Lukas Ružauskas <nequadora@gmail.com>
2023-06-11 23:19:41 +0300

    Tried fixing toaster

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,6 +1,7 @@
 import { ToastContainer } from "react-toastify";
 import "./../globals.css";
 import { Inter } from "next/font/google";
+import "react-toastify/dist/ReactToastify.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,7 @@ export default async function AdminLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <ToastContainer />
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
